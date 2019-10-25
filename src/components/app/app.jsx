608722662ac12449e5e9cbcd3d05cd7eafe34772
dragooon4ik24 <shafner.anton@gmail.com>
2019-10-25 10:50:51 +0300

    создал компоненту карточка и добавил валидацию пропсов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MainScreen from '../main-screen/main-screen.jsx';
 
-const App = (data) => {
-  const {arrDescriptions} = data;
+const App = ({arrDescriptions}) => {
   return <MainScreen
     arrDescriptions={arrDescriptions}
   />;
+};
+
+App.propTypes = {
+  arrDescriptions: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default App;
